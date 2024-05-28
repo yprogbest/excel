9e75c728f9c5d6275a09124da6379f3c4c04c9ed
YProg <82224433+yprogbest@users.noreply.github.com>
2024-05-28 15:21:06 +0900

    Add files via upload

--- code_ver1/test_ver1.py
+++ code_ver1/test_ver1.py
@@ -16,7 +16,13 @@ if __name__ == "__main__":
     # 各シートをリストに格納
     each_date_sheet_list = []
     for i in range(12, 17):
-        each_date_sheet = wb_temp[f'2024_04_{i}']
+        sheet_name = f'2024_04_{i}' #シートの名前
+
+        if sheet_name in wb.sheetnames:
+            each_date_sheet = wb_temp[f'2024_04_{i}']
+        else:
+            each_date_sheet = "NoneSheet"
+
         each_date_sheet_list.append(each_date_sheet)
 
 
@@ -29,57 +35,65 @@ if __name__ == "__main__":
 
         dic = {}
         col = 2
-        while(str(each_date_sheet_list[j].cell(column=col, row=10).value) != "None"): #列方向
 
-            row_ = 11
+        # 各日のシートが存在するなら
+        if each_date_sheet_list[j] != "NoneSheet":
 
-            #リストの削除
-            value_list = []  # 新しいリストを作成
-            while(str(each_date_sheet_list[j].cell(column=col, row=row_).value) != "None"):
+            while(str(each_date_sheet_list[j].cell(column=col, row=10).value) != "None"): #列方向
 
-                value_list.append(each_date_sheet_list[j].cell(column=col, row=row_).value) #値を取得
+                row_ = 11
 
+                #リストの削除
+                value_list = []  # 新しいリストを作成
+                while(str(each_date_sheet_list[j].cell(column=col, row=row_).value) != "None"):
 
-                row_ +=1
-
-            #辞書にキーと値を格納
-            dic[each_date_sheet_list[j].cell(column=col, row=10).value] = value_list
-
-            col += 1
+                    value_list.append(each_date_sheet_list[j].cell(column=col, row=row_).value) #値を取得
 
 
-        # print(f'dic = {dic}')
-        # print(f'dic size = {len(dic)}')
+                    row_ +=1
+
+                #辞書にキーと値を格納
+                dic[each_date_sheet_list[j].cell(column=col, row=10).value] = value_list
+
+                col += 1
 
 
-        
-        #辞書のキーを用いて、Sheet1のB列を探索
-        for key, value in dic.items():
-
-            flag = False
-            for row in ws_temp.iter_rows(min_col=2, max_col=2, min_row=1, max_row=ws_temp.max_row): #B列の1行目から一番下までをfor文で走査する
-                for cell in row:
-
-                    if cell.value == key: #もし、Sheet1のB列の値と辞書のキーが等しいなら
-
-                        col_main = cell.column + (j+1)
-                        row_ = cell.row + 2
-                        for i in range(len(value)): #統計値をSheet1に入力していく
-                            ws.cell(column=col_main, row=row_).value = value[i]
-                            row_ += 1
+            # print(f'dic = {dic}')
+            # print(f'dic size = {len(dic)}')
 
 
+            
+            #辞書のキーを用いて、Sheet1のB列を探索
+            for key, value in dic.items():
+
+                flag = False
+                for row in ws_temp.iter_rows(min_col=2, max_col=2, min_row=1, max_row=ws_temp.max_row): #B列の1行目から一番下までをfor文で走査する
+                    for cell in row:
+
+                        if cell.value == key: #もし、Sheet1のB列の値と辞書のキーが等しいなら
+
+                            col_ = cell.column + (j+1)
+                            row_ = cell.row + 2
+
+                            #もし、日時の下のデータの値が0でないなら、
+                            if ws.cell(column=col_, row=4).value != 0:
+                                for i in range(len(value)): #統計値をSheet1に入力していく
+                                    ws.cell(column=col_, row=row_).value = value[i]
+                                    row_ += 1
 
 
-                        # print(f'cell.column = {cell.column}') #セル番地の取得
-                        # print(f'cell.row = {cell.row}') #セル番地の取得
-                        # print(f'cell.value = {cell.value}') # セルの値
-                        flag = True
 
+
+                            # print(f'cell.column = {cell.column}') #セル番地の取得
+                            # print(f'cell.row = {cell.row}') #セル番地の取得
+                            # print(f'cell.value = {cell.value}') # セルの値
+                            flag = True
+
+                            break
+
+                    if flag == True:
                         break
 
-                if flag == True:
-                    break
 
 
 
